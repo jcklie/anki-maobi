242703a5fe786639c6948379caabb2a4a50db635
Jan-Christoph Klie <git@mrklie.com>
2020-09-06 14:28:16 +0200

    #36 - Configure decks and note types gives error messages
    - Adapt to new Anki version
    - Add new hook for adding config buttons

--- maobi/__init__.py
+++ maobi/__init__.py
@@ -1,26 +1,33 @@
 from anki.hooks import wrap
 
 
-def patch_quiz():
-    from .quiz import maobi_hook
+def hook_quiz():
+    from .quiz import maobi_review_hook
 
     try:
         # Anki >= 2.1.20
         from aqt import gui_hooks
-        gui_hooks.card_will_show.append(maobi_hook)
+
+        gui_hooks.card_will_show.append(maobi_review_hook)
     except:
         # Use the legacy hook instead
         from anki.hooks import addHook
-        addHook("prepareQA", maobi_hook)
+
+        addHook("prepareQA", maobi_review_hook)
 
 
-def patch_config():
-    from aqt.clayout import CardLayout
+def hook_add_config_button():
+    try:
+        from aqt import gui_hooks
+        from .config import maobi_add_config_button_hook
 
-    from .config import add_maobi_button
+        gui_hooks.card_layout_will_show.append(maobi_add_config_button_hook)
+    except:
+        from aqt.clayout import CardLayout
+        from .config import maobi_add_config_button_legacy_patch
 
-    CardLayout.setupButtons = wrap(CardLayout.setupButtons, add_maobi_button)
+        CardLayout.setupButtons = wrap(CardLayout.setupButtons, maobi_add_config_button_legacy_patch)
 
 
-patch_quiz()
-patch_config()
+hook_quiz()
+hook_add_config_button()
